40b8da56a33d4200746433d0209e5f0705d06035
Kevin Sasse <sassek70@gmail.com>
2022-10-11 16:22:04 -0400

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,20 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div>
+      <h2 id="about">About</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
